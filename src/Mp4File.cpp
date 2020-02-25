901d19610860040f20545f5ec373494560c12128
Stefan Götschi <stefan@stefan-goetschi.ch>
2020-02-25 21:11:07 +0100

    Parse mvhd header.

--- src/Mp4File.cpp
+++ src/Mp4File.cpp
@@ -1,6 +1,7 @@
 #include "Mp4File.h"
 
 #include <array>
+#include <cassert>
 #include <fstream>
 #include <vector>
 
@@ -44,31 +45,96 @@ class BlockHandler {
 using BlockHandlerPtr = std::unique_ptr<BlockHandler>;
 class SkipBlockHandler : public BlockHandler {
   bool canHandle(std::string const& /*type*/) const override { return true; }
-  void handle(std::fstream& /*stream*/, uint32_t /*size*/) override {};
+  void handle(std::fstream& /*stream*/, uint32_t /*size*/) override{};
 };
-class HeaderBlockHandler : public BlockHandler {
+class MvhdHeaderHandler : public BlockHandler {
+ public:
+  MvhdHeaderHandler(Mp4File::Metadata& metadata) : metadata(metadata) {}
   bool canHandle(std::string const& type) const override {
-    return FILE_HEADER == type;
+    return "mvhd" == type;
   }
-  void handle(std::fstream& /*stream*/, uint32_t /*size*/) override {
-    // TODO: Read header
+  void handle(std::fstream& stream, uint32_t size) override {
+    auto const pos = stream.tellg();
+    auto const version = streamRead<uint8_t>(stream, Endian::LITTLE);
+    auto const empty1 = streamRead<uint8_t>(stream, Endian::LITTLE);
+    auto const empty2 = streamRead<uint16_t>(stream, Endian::LITTLE);
+    auto const created = streamRead<uint32_t>(stream, Endian::BIG);
+    metadata.created = static_cast<std::time_t>(created - 2082844800u);
+    auto const modified = streamRead<uint32_t>(stream, Endian::BIG);
+    metadata.modified = static_cast<std::time_t>(modified - 2082844800u);
+    auto const timeScale = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const timeLength = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const playbackSpeed = streamRead<int32_t>(stream, Endian::BIG);
+    auto const volume = streamRead<int16_t>(stream, Endian::BIG) / 256.;
+    auto const empty3 = streamRead<int64_t>(stream, Endian::LITTLE) / 65536.;
+    auto const empty4 = streamRead<int16_t>(stream, Endian::LITTLE) / 65536.;
+    auto const geoMatrixA = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixB = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixU = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixC = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixD = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixV = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixX = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixY = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const geoMatrixW = streamRead<int32_t>(stream, Endian::BIG) / 65536.;
+    auto const previewStart = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const previewLength = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const posterFrame = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const selectionStart = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const selectionLength = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const currentTime = streamRead<uint32_t>(stream, Endian::BIG);
+    auto const nextTrackId = streamRead<uint32_t>(stream, Endian::BIG);
+    assert(stream.tellg() == pos + static_cast<std::streampos>(size - 8));
   };
+
+ private:
+  Mp4File::Metadata& metadata;
 };
 
 template <size_t N>
-BlockHandler& getHandler(std::array<BlockHandlerPtr, N>& handlers, std::fstream& stream) {
+BlockHandler& getHandler(std::array<BlockHandlerPtr, N>& handlers,
+                         std::fstream& stream) {
   auto const blockType = std::string(
       streamRead<std::array<char, 4>>(stream, Endian::LITTLE).data(), 4);
   auto handlerIter = std::find_if(handlers.begin(), handlers.end(),
-                                  [blockType](BlockHandlerPtr const& handler) {
+                                  [&blockType](BlockHandlerPtr const& handler) {
                                     return handler->canHandle(blockType);
                                   });
   if (handlerIter == handlers.end())
     throw std::exception("Unxpectedly no block handler available.");
   return **handlerIter;
 }
+
+class HeaderBlockHandler : public BlockHandler {
+ public:
+  HeaderBlockHandler(Mp4File::Metadata& metadata) : metadata(metadata) {}
+  bool canHandle(std::string const& type) const override {
+    return FILE_HEADER == type;
+  }
+  void handle(std::fstream& stream, uint32_t size) override {
+    std::array<BlockHandlerPtr, 2> handlers{
+        std::make_unique<MvhdHeaderHandler>(metadata),
+        std::make_unique<SkipBlockHandler>()};
+
+    auto const endPos = stream.tellg() + static_cast<std::streampos>(size - 8);
+    while (stream.tellg() < endPos) {
+      auto const position = stream.tellg();
+      auto const blockSize = streamRead<uint32_t>(stream, Endian::BIG);
+      auto& handler = getHandler(handlers, stream);
+      handler.handle(stream, blockSize);
+
+      if (stream.tellg() != position + static_cast<std::streampos>(blockSize))
+        stream.seekg(position + static_cast<std::streampos>(blockSize));
+    }
+  }
+
+ private:
+  Mp4File::Metadata& metadata;
+};
+
 template <size_t N>
-void parseBlocks(std::fstream& stream, std::array<BlockHandlerPtr, N>& handlers) {
+void parseBlocks(std::fstream& stream,
+                 std::array<BlockHandlerPtr, N>& handlers) {
   while (stream.peek() != std::char_traits<char>::eof()) {
     auto const position = stream.tellg();
     auto const blockSize = streamRead<uint32_t>(stream, Endian::BIG);
@@ -79,10 +145,9 @@ void parseBlocks(std::fstream& stream, std::array<BlockHandlerPtr, N>& handlers)
 }
 }  // namespace
 
-namespace MetaMovie {
 Mp4File::Mp4File(std::string const& path) : path(path) {
   std::array<BlockHandlerPtr, 2> blockHandlers{
-      std::make_unique<HeaderBlockHandler>(),
+      std::make_unique<HeaderBlockHandler>(metadata),
       std::make_unique<SkipBlockHandler>()};
 
   ManagedFile file(path, std::ios_base::in | std::ios_base::binary);
--- src/Mp4File.h
+++ src/Mp4File.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <chrono>
+#include <ctime>
 #include <string>
 
 
@@ -8,8 +8,14 @@ namespace MetaMovie {
 class Mp4File final {
  public:
   Mp4File(std::string const& path);
- 
+
+  struct Metadata {
+    std::time_t created;
+    std::time_t modified;
+  };
+
  private:
   std::string path;
+  Metadata metadata;
 };
 }  // namespace MetaMovie
